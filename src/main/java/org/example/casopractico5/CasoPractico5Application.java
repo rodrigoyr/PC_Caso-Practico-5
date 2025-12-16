498544037d6e83f17b9e51cf10706b6f2a1c9c2d
Rodrigo <146821758+rodrigoyr@users.noreply.github.com>
2025-12-16 19:26:58 +0100

    commit

---
+++ src/main/java/org/example/casopractico5/CasoPractico5Application.java
@@ -0,0 +1,13 @@
+package org.example.casopractico5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasoPractico5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasoPractico5Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/casopractico5/CasoPractico5ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.casopractico5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasoPractico5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
